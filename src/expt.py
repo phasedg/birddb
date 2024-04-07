76edb013bbd47f3cf9af3e1128853dc763bf69b4
dg <phasedance@gmail.com>
2024-04-07 09:01:05 -0400

    fixed checkpointing

--- src/expt.py
+++ src/expt.py
@@ -12,114 +12,58 @@ import torch.nn as nn
 import torch.optim as optim
 import matplotlib.pyplot as plt
 import pandas as pd
+import pickle
+import glob
 
 '''
   Expt subclasses encapsule a model structure, training regime
   Instances add training dataset, and paths
 '''
 class Expt:
-    def __init__(self):
-        pass
-
-    def loadModel(self):
-        self.model = models.RN50_Bird_V1.loadModel(self.expdir,'model.pt')
-        return self.model
-
-    def testOnDataset(self,ds):
-        since = time.time()
-        criterion = nn.CrossEntropyLoss()
-        best_acc = 0.0
-        dl = ds.makeDataloader(batch_size=32, shuffle=False)
-        self. model.to(self.device)
-        # Each epoch has a training and validation phase
-        self.model.eval()   # Set model to evaluate mode
-
-        running_loss = 0.0
-        running_corrects = 0
-        with torch.no_grad():
-            # Iterate over data.
-            for inputs, labels in dl:
-                inputs = inputs.to(self.device)
-                labels = labels.to(self.device)
-
-                outputs = self.model(inputs)
-                _, preds = torch.max(outputs, 1)
-                loss = self.criterion(outputs, labels)
-
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-                # print(outputs.shape,preds.shape,labels.data.shape)
-                running_corrects += torch.sum(preds == labels)
-        epoch_loss = running_loss / len(ds)
-        epoch_acc = running_corrects.double() / len(ds)
-        print(f'Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
-
-        time_elapsed = time.time() - since
-        print(f'Testing complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
-        return epoch_loss, epoch_acc
-
-    def visualizeOnDataset(self,ds, nameMap = None,num_images=6):
-        dl = ds.makeDataloader(batch_size=32, shuffle=True)
-        self.model.to(self.device)
-        self.model.eval()
-        images_so_far = 0
-        fig = plt.figure(figsize=(10,10))
-        if nameMap is None:
-            nameMap = ds.getClassName
-        with torch.no_grad():
-            for i, (inputs, labels) in enumerate(dl):
-                inputs = inputs.to(self.device)
-                labels = labels.to(self.device)
-                outputs = self.model(inputs)
-                _, preds = torch.max(outputs, 1)
-                
-                for j in range(inputs.size()[0]):
-                    images_so_far += 1
-                    ax = plt.subplot(num_images//2, 2, images_so_far)
-                    ax.axis('off')
-                    ax.set_title(f'label: {nameMap(labels[j])} \npredicted:{ nameMap(preds[j])}',fontsize=8)
-                    ax.imshow(inputs.cpu().data[j].permute(1,2,0))
-                    if images_so_far == num_images:
-                        break
-                if images_so_far == num_images:
-                    break
-        plt.show()
-
-
-
-class Expt_RN50_V1_T1:
-    def __init__(self,expdir,datadir,dbname,device):
+    def __init__(self,expdir,datadir,dbname,device,trainTrans):
         self.datadir = datadir
         self.dbname = dbname
         self.dbdir = Path(datadir / dbname)
-        self.trainTrans = transforms.Compose([
-            transforms.RandomHorizontalFlip(),
-            #transforms.ColorJitter(brightness=.3),
-            # transforms.RandomRotation(20),
-            #    transforms.CenterCrop(224),
-        ])
+        self.expdir = expdir
+        if not os.path.exists(self.expdir):
+            os.makedirs(self.expdir)
 
+        self.device = device
+        self.chkpath = self.expdir + '/check.pt'
+        self.model = None
+        self.trainTrans = trainTrans
         self.trainds = birddb.TrainImageDataset(self.dbdir,transform=self.trainTrans)
         self.trainds.name = f'Train_{dbname}'
-        self.traindl = self.trainds.makeDataloader(batch_size=32,shuffle=True)
-        self.expdir = expdir
-        self.device = device
+        self.trainer = None
+
+    def dirDict(self):
+        return {
+            'datadir': self.datadir,
+            'dbname': self.dbname,
+            'expdir': self.expdir,
+            'chkpath': self.chkpath,
+            'dbdir': self.dbdir
+            }
+
+    def callback(self,epoch):
+        if (epoch+1) % 5 == 0:
+            self.trainer.checkpoint(self.chkpath)
+
+    def hasCheckpoint(self):
+        print(self.chkpath)
+        return os.path.isfile(self.chkpath)
+    
+    def trainModel(self,epochs,write=True,dotfreq=10):
+        self.trainer = self.buildTrainer()
+        self.trainer.dotfreq=dotfreq
+        if self.hasCheckpoint():
+            self.trainer.restore(self.chkpath)
+        losses, accs = self.trainer.train(epochs,self.callback)
+        self.trainer..model.writeModel(self.expdir,'model.pt')
+        df = pd.DataFrame({'loss': losses, 'acc': accs})
+        df.to_csv(str(self.expdir) + '/train_acc.csv',index=False)
 
 
-        self.model = models.RN50_Bird_V1('RN50_V1' + '-' + self.trainds.name,self.trainds.numClasses())
-        self.criterion = nn.CrossEntropyLoss()
-        self.optimizer = optim.SGD([{'params':self.model.bird_model.parameters()},
-                            {'params':self.model.rn50_model.avgpool.parameters()},
-                            {'params':self.model.rn50_model.layer4.parameters()}],
-
-                                   lr=0.01, momentum=0.9)
-        # Decay LR by a factor of 0.1 every 7 epochs
-        self.scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size=10, gamma= 0.5)
-        self.trainer = Trainer(self.model, self.device, self.traindl, None, self.criterion, self.optimizer, self.scheduler, writer=None,dotfreq=0, chkpath=self.expdir + '/check.pt')
-
-    def trainModel(self,epochs,write=True):
-        losses, accs = self.trainer.train(epochs)
-        self.model.writeModel(self.expdir,'model.pt')
 
     def loadModel(self):
         self.model = models.RN50_Bird_V1.loadModel(self.expdir,'model.pt')
@@ -185,59 +129,92 @@ class Expt_RN50_V1_T1:
                     break
         plt.show()
 
-    
+    def crossTest(self,dbs):
+        stats = {}
+        statfile = self.expdir + '/stats.pkl'
+        if os.path.exists(statfile):
+            with open(statfile,'rb') as f:
+                stats = pickle.load(f)
+        expbase = os.path.dirname(self.expdir)  #strip train db level 
+        for db in dbs:
+            if db not in stats:
+                stats[db] = {}
+            for test in ['train', 'test']:
+                if test not in stats[db]:
+                    print(f'{test} not in stats[{db}]...computing')
+                    stats[db][test] = {}
+                    dbdir = Path(self.datadir / db)
+                    if db[0] == self.dbname[0]: # quicl test
+                        testds = birddb.TestImageDataset(dbdir,self.trainds,trainset=test)
+                    else:
+                        testds = birddb.CrossImageDataset(dbdir,self.trainds,trainset=test)
+                    testds.printStats()
+                    print(f'db: {db}, test: {test}')
+                    #myexpt.visualizeOnDataset(testds)
+                    loss, acc = self.testOnDataset(testds)
+                    stats[db][test]= acc.item()
+                    with open(statfile,'wb') as f:
+                        pickle.dump(stats,f)
+
+        rows = []
+        for fname in glob.glob(expbase + '/*/*.pkl'):
+            dbname = fname.split('/')[-2]
+            print(dbname)
+            with open(fname,'rb') as f:
+                stats = pickle.load(f)
+                print(stats)
+                for x in stats:
+                    row = [dbname,x,stats[x]['train'],stats[x]['test']]
+                    rows.append(row)
+        df = pd.DataFrame(rows,columns=['traindb','testdb','trainacc','testacc'])
+        df.to_csv(expbase + '/stats.csv')
+        return df
+
+
 class Expt_RN50_V1_T2(Expt):
-    def __init__(self,expdir,datadir,dbname,device):
-        super().__init__()
-        self.datadir = datadir
-        self.dbname = dbname
-        self.dbdir = Path(datadir / dbname)
-        self.trainTrans = transforms.Compose([
+    def __init__(self,expbase,datadir,dbname,device):
+        trainTrans = transforms.Compose([
             transforms.AutoAugment(),
             #transforms.RandomHorizontalFlip(),
             #transforms.ColorJitter(brightness=.3),
             # transforms.RandomRotation(20),
             #    transforms.CenterCrop(224),
         ])
-
-        self.trainds = birddb.TrainImageDataset(self.dbdir,transform=self.trainTrans)
-        self.trainds.name = f'Train_{dbname}'
-        self.traindl = self.trainds.makeDataloader(batch_size=32,shuffle=True)
-        self.expdir = expdir
-        self.device = device
-
-
-        self.model = models.RN50_Bird_V1('RN50_V1_' + '-' + self.trainds.name,self.trainds.numClasses())
+        self.expbase = expbase
+        super().__init__(str(expbase) + '/RN50_T1/T2/'+ dbname,datadir,dbname,device,trainTrans)
         self.criterion = nn.CrossEntropyLoss()
+
+
+
+    def buildTrainer(self):
+        self.traindl = self.trainds.makeDataloader(batch_size=32,shuffle=True)
+        self.model = models.RN50_Bird_V1('RN50_V1_' + self.trainds.name,self.trainds.numClasses())
         self.optimizer = optim.SGD([{'params':self.model.bird_model.parameters()},
                             {'params':self.model.rn50_model.avgpool.parameters()},
                             {'params':self.model.rn50_model.layer4.parameters()}],
 
                                    lr=0.01, momentum=0.9)
         # Decay LR by a factor of 0.1 every 7 epochs
-        self.scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size=2, gamma= 0.9)
-        self.trainer = Trainer(self.model, self.device, self.traindl, None, self.criterion, self.optimizer, self.scheduler, writer=None,dotfreq=0, chkpath=self.expdir + '/check.pt')
-
-    def trainModel(self,epochs,write=True):
-        losses, accs = self.trainer.train(epochs)
-        self.model.writeModel(self.expdir,'model.pt')
-        df = pd.DataFrame({'loss': losses, 'acc': acc})
-        df.to_csv(str(self.expdir) + '/train_acc.csv')
+        self.scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size=4, gamma= 0.9)
+        self.trainer = Trainer(self.model, self.device, self.traindl, None, self.criterion, self.optimizer, self.scheduler, writer=None)
+        return self.trainer
 
 
 
 if __name__ == "__main__":
     datadir = Path('/data1/datasets/birds')
     dbname = 'CUB_wood_sm'
-    expdir = str(Path('/home/dg/proj/birddb/expts/RN50_V1_' + dbname))
-    if not os.path.exists(expdir):
-        os.makedirs(expdir)
+    expbase = '/home/dg/proj/birddb/expts'
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    expt = Expt_RN50_V1_T1(expdir,datadir,dbname,device)
+    expt = Expt_RN50_V1_T2(expbase,datadir,dbname,device)
+    print(expt.dirDict())
+    expt.trainModel(5)
 
-#    expt.trainModel(10)
-#    exit()
+    df = pd.read_csv(expt.expdir + '/train_acc.csv')
+    print(df)
+    df.plot()
+    exit()
     dbdir = Path(datadir / dbname)
 
     model = expt.loadModel()
--- src/trainer.py
+++ src/trainer.py
@@ -5,7 +5,7 @@ import torch
 
 
 class Trainer:
-    def __init__(self,model, device, traindl, valdl, criterion, optimizer, scheduler, writer,dotfreq=0, chkpath='check.pt'):
+    def __init__(self,model, device, traindl, valdl, criterion, optimizer, scheduler, writer,dotfreq=0):
         self.model = model
         self.criterion = criterion
         self.scheduler = scheduler
@@ -16,7 +16,9 @@ class Trainer:
         self.device = device
         self.dotfreq = dotfreq
         self.epoch = 0
-        self.chkpath = chkpath
+        self.losses = []
+        self.accs = []
+
 
         #training loop from pytorchs docs
     def train_one_epoch(self,epoch_index):
@@ -53,7 +55,7 @@ class Trainer:
                 
                 # Gather data and report
             running_loss += loss.item() * inputs.size(0)
-            running_correct += torch.sum(preds == labels)                
+            running_correct += torch.sum(preds == labels).item()        
                 
         last_loss = running_loss / len(self.traindl.dataset) # loss per batch
         last_acc = running_correct / len(self.traindl.dataset) # loss per batch
@@ -86,55 +88,61 @@ class Trainer:
                 
                 # Gather data and report
                 running_loss += loss.item()
-                running_correct += torch.sum(preds == labels)                
+                running_correct += torch.sum(preds == labels).item()          
         last_loss = running_loss / len(self.valdl.dataset) # loss per batch
         last_acc = running_correct / len(self.valdl.dataset) # loss per batch
 
         return last_loss, last_acc
 
-    def train(self,epochs):
+    def train(self,epochs,callback=None):
 #        timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')            
-        losses = []
-        accs = []
         self.model = self.model.to(self.device)
-        for i in range(self.epoch,epochs):
+        start = self.epoch
+        for i in range(start,epochs):
             print(f'Epoch {i}: ',end='')
             loss,acc = self.train_one_epoch(i)
             if self.scheduler is not None:
                self.scheduler.step()
-            if ((i+1)%5) == 0:
-                if self.chkpath is not None:
-                    self.checkpoint(i)
-                if (self.valdl is not None):
-                    tloss,tacc = self.val_one_epoch(i)                
-                    print(f' Train: Loss:{loss:.3f} Acc:{acc:.3f} Test: Loss:{tloss:.3f} Acc:{tacc:.3f}')
-                else:
-                    print(f' Train: Loss:{loss:.3f} Acc:{acc:.3f}')
+            self.losses.append(loss)
+            self.accs.append(acc)
+            self.epoch = i+1  # epoch to restart at
+            if callback is not None:
+                callback(i)
+            if (self.valdl is not None) and ((i+1)%5) == 0:
+                tloss,tacc = self.val_one_epoch(i)                
+                print(f' Train: Loss:{loss:.3f} Acc:{acc:.3f} Test: Loss:{tloss:.3f} Acc:{tacc:.3f}')
             else:
                 print(f' Train: Loss:{loss:.3f} Acc:{acc:.3f}')
             if self.writer is not None:
                 self.writer.add_scalar('Train/Loss:', loss,i)
                 self.writer.add_scalar('Train/Acc:', acc,i)
-            losses.append(loss)
-            accs.append(acc)
-            
-        return losses,accs
+        return self.losses,self.accs
 
-    def checkpoint(self,epoch):
-        torch.save({
-            'epoch': epoch,
-            'model_state': self.model.state_dict(),
-            'opt_state': self.optimizer.state_dict(),
-            'sched_state': self.scheduler.state_dict()
-            }, self.chkpath)
-
-    def restore(self):
-        cpt = torch.load(self.chkpath,map_location='cpu')
+        
+    '''
+      Restore state from checkpoint file. Note: checkpoitn stores state not structure so trainer must be rebuilt with __init__ before restoring
+    '''
+    def restore(self,chkpath):
+        cpt = torch.load(chkpath,map_location=self.device)
         self.model.load_state_dict(cpt['model_state'])
+        self.model.to(self.device)
         self.optimizer.load_state_dict(cpt['opt_state'])
         self.scheduler.load_state_dict(cpt['sched_state'])
         self.epoch = cpt['epoch']
-        print(f'return to epoch {self.epoch}')
+        if 'losses' in cpt:
+            self.losses = cpt['losses']
+            self.accs = cpt['accs']
+        print(f'restore to epoch {self.epoch}')
 
-        
-
+    '''
+     Stores trainer state (not structure)
+    '''
+    def checkpoint(self,chkpath):
+        torch.save({
+            'epoch': self.epoch,
+            'model_state': self.model.state_dict(),
+            'opt_state': self.optimizer.state_dict(),
+            'sched_state': self.scheduler.state_dict(),
+            'losses': self.losses,
+            'accs': self.accs
+        }, chkpath)
